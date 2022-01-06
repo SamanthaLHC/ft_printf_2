6588e2ed5b68d43b1a414be83bb6a059d67f4b26
Samantha LE HUEC <sle-huec@e1r13p7.42.fr>
2022-01-06 13:19:31 +0100

    main clean

--- main.c
+++ main.c
@@ -9,51 +9,44 @@ int main()
 	char	e = 'a';
 	int len;
 
-	printf("\n \n >> retour: %d << \n",printf(" print char :%c",e));
+	printf("\n >> retour: %d << \n",printf(" print char :%c",e));
 	ft_printf("\n retour ft: %d <<\n", ft_printf(" print char :%c", e));
-}	
-/*	
-	printf("\n retour: %d <<\n",printf("%s",c));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%s",c));
+	
+	printf("\n retour: %d <<\n",printf(" print str %s",c));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf(" print str %s",c));
 	printf("\n");
 	
-
-	printf("\n retour: %d <<\n",printf("%s, %c",c,e));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%s, %c",c,e));
+	printf("\n retour: %d <<\n",printf(" print str + char %s, %c",c,e));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf(" print str + char %s, %c",c,e));
 	printf("\n");
 
-	
-	// avec un segfault: printf("%s\n %c\n", e, c);
-	//avec un segfault: ft_printf("%s\n %c\n", e, c);
-	
-	printf("\n retour: %d <<",printf("%i", 0));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%i", 0));
+	printf("\n retour: %d <<",printf(" print modulo i %i", 0));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf(" print modulo i %i", 0));
 	printf("\n");
 	
-	printf("\n retour: %d <<\n",printf("%%"));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%%"));
+	printf("\n retour: %d <<\n",printf(" print modulo %%"));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf(" print modulo %%"));
 	printf("\n");
 	
-	printf("\n retour: %d <<\n",printf("%u", -1234));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%u", -1234));
+	printf("\n retour: %d <<\n",printf(" modulo u %u", 1234));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf(" modulo u %u", 1234));
 	printf("\n");
 	
 	
-	printf("\n retour: %d <<\n",printf("%u", 2147473647));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%u", 2147473647));
+	printf("\n retour: %d <<\n",printf("int max avec modulo u %u", 2147473647));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf("int max avec modulo u %u", 2147473647));
 	printf("\n");
 	
 	
-	printf("\n retour: %d <<i\n",printf("%x",2147483647));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%x",2147483647));
+	printf("\n retour: %d <<i\n",printf("modulo x %x", 2147483647));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf("modulo x %x", 2147483647));
 	printf("\n");
 	
-	printf("\n retour: %d <<\n",printf("%X",2147473647));
-	ft_printf("\n retour: %d <<\n",ft_printf("%X",2147473647));
+	printf("\n retour: %d <<\n",printf("modulo X %X",21477));
+	ft_printf("\n retour: %d <<\n",ft_printf("modulo X %X",21477));
 	printf("\n");
 	
-	printf("\n retour: %d <<\n",printf("%p", c));
-	ft_printf("\n retour avec ft: %d <<\n",ft_printf("%p", c));
+	printf("\n retour: %d <<\n",printf("print ptr %p", c));
+	ft_printf("\n retour avec ft: %d <<\n",ft_printf("print ptr %p", c));
 	printf("\n");
 }
-*/
